d24d8fbc9eb618ea88d2ffbf090924f48f65e669
Augustors <augusto7410@gmail.com>
2022-09-07 17:05:25 -0300

    first commit - Data Cleaning

---
+++ index.py
@@ -0,0 +1,41 @@
+from re import template
+import dash
+from dash import html, dcc, Input, Output, State
+import dash_bootstrap_components as dbc
+import pandas as pd
+import numpy as np
+import plotly.express as px
+from plotly import graph_objects as go
+from dash_bootstrap_templates import ThemeSwitchAIO
+
+#======== APP ==========#
+FONT_AWESOME = ["https://use.fontawesome.com/releases/v5.10.2/css/all.css"]
+dbc_css = "https://cdn.jsdelivr.net/gh/AnnMarieW/dash-bootstrap-templates@V1.0.4/dbc.min.css"
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.FLATLY, dbc_css])
+app.scripts.config.serve_locally = True
+server = app.server
+
+#======== Styles ==========#
+template_theme1 = "flatly"
+template_theme2 = "vapor"
+url_theme1 = dbc.themes.FLATLY
+url_theme2 = dbc.themes.VAPOR
+
+#======== Reading and Cleaning File ==========#
+df_main = pd.read_csv("data_gas.csv")
+
+df_main['DATA INICIAL'] = pd.to_datetime(df_main['DATA INICIAL'])
+df_main['DATA FINAL'] = pd.to_datetime(df_main['DATA FINAL'])
+df_main['DATA'] = ((df_main['DATA FINAL'] - df_main['DATA INICIAL'])/2) + df_main['DATA INICIAL']
+
+df_main = df_main.sort_values(by='DATA', ascending= True)
+df_main.rename(columns={"PREÇO MÉDIO REVENDA": "VALOR REVENDA (R$/L)"}, inplace=True)
+
+
+
+#======== Layout ==========#
+app.layout = dbc.Container(children=[
+
+
+], fluid=True, style = {"height":"100%"})
