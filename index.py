7319e5ace8b34c3dfe33e82ecd038ace67a55076
Augustors <augusto7410@gmail.com>
2022-09-07 19:08:18 -0300

    Linha 1

--- index.py
+++ index.py
@@ -1,4 +1,3 @@
-from re import template
 import dash
 from dash import html, dcc, Input, Output, State
 import dash_bootstrap_components as dbc
@@ -22,6 +21,8 @@ template_theme2 = "vapor"
 url_theme1 = dbc.themes.FLATLY
 url_theme2 = dbc.themes.VAPOR
 
+tab_card = {"height": "100%"}
+
 #======== Reading and Cleaning File ==========#
 df_main = pd.read_csv("data_gas.csv")
 
@@ -32,10 +33,99 @@ df_main['DATA'] = ((df_main['DATA FINAL'] - df_main['DATA INICIAL'])/2) + df_mai
 df_main = df_main.sort_values(by='DATA', ascending= True)
 df_main.rename(columns={"PREÇO MÉDIO REVENDA": "VALOR REVENDA (R$/L)"}, inplace=True)
 
+df_main["ANO"] = df_main["DATA"].apply(lambda x: str(x.year))
+df_main = df_main[df_main.PRODUTO == "GASOLINA COMUM"]
+df_main = df_main.reset_index()
+
+df_main.drop(['UNIDADE DE MEDIDA', 'COEF DE VARIAÇÃO REVENDA', 'COEF DE VARIAÇÃO DISTRIBUIÇÃO',
+    'NÚMERO DE POSTOS PESQUISADOS', 'DATA INICIAL', 'DATA FINAL', 'PREÇO MÁXIMO DISTRIBUIÇÃO', 'PREÇO MÍNIMO DISTRIBUIÇÃO', 'DESVIO PADRÃO DISTRIBUIÇÃO',
+    'MARGEM MÉDIA REVENDA', 'PREÇO MÍNIMO REVENDA', 'PREÇO MÁXIMO REVENDA', 'DESVIO PADRÃO REVENDA',
+    'PRODUTO', 'PREÇO MÉDIO DISTRIBUIÇÃO'], inplace = True, axis = 1)
+
+df_store = df_main.to_dict()
 
 
 #======== Layout ==========#
 app.layout = dbc.Container(children=[
+    #Armazenar o Dataset
+    dcc.Store(id='dataset', data=df_store),
+    dcc.Store(id='dataset_fixed', data=df_store),
 
-
+    #Layout
+    #Row 1
+    dbc.Row([
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            html.Legend("Gas Prices Analysis")
+                        ], sm=8),
+                        dbc.Col([
+                            html.I(className='fa fa-filter', style={'font-size': '300%'})
+                        ], sm=4, align='center')
+                    ]),
+                    dbc.Row([
+                        dbc.Col([
+                            ThemeSwitchAIO(aio_id='theme', themes=[url_theme1, url_theme2])                   
+                        ])
+                    ])
+                ])
+            ], style = tab_card)
+        ], sm= 4, lg=2),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            html.H3('Máximos e Mínimos'),
+                            dcc.Graph(id='static-maxmin', config={"displayModeBar": False, "showTips": False})
+                        ])
+                    ])
+                ])
+            ], style = tab_card)
+        ], sm=8, lg=3),
+        dbc.Col([
+            dbc.Card([
+                dbc.CardBody([
+                    dbc.Row([
+                        dbc.Col([
+                            html.H6('Ano de Análise:'),
+                            dcc.Dropdown(
+                                id="select-ano",
+                                value=df_main.at[df_main.index[1],'ANO'],
+                                clearable=False,
+                                className='dbc',
+                                options=[
+                                    {"label": x, "value": x} for x in df_main.ANO.unique()
+                                ]),
+                        ], sm=6),
+                        dbc.Col([
+                            html.H6('Região de Análise'),
+                            dcc.Dropdown(
+                                id='select-regiao',
+                                value=df_main.at[df_main.index[1],'REGIÃO'],
+                                clearable=False,
+                                className='dbc',
+                                options=[
+                                    {"label": x, "value": x} for x in df_main.REGIÃO.unique()
+                                ]),
+                        ], sm=6)
+                    ]),
+                    dbc.Row([
+                        dbc.Col([
+                            dcc.Graph(id='regiaobar_graph', config={"displayModeBar": False, "showTips": False})
+                        ], sm=12, md=6),
+                        dbc.Col([
+                            dcc.Graph(id='estadobar_graph', config={"displayModeBar": False, "showTips": False})
+                        ], sm=12, md=6)
+                    ], style={'column-gap': '0px'})
+                ])
+            ], style=tab_card)
+        ], sm=12, lg=7)
+    ]),
 ], fluid=True, style = {"height":"100%"})
+
+# Run server
+if __name__ == '__main__':
+    app.run_server(debug=True)
